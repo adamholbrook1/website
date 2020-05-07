232c1ccdb93ccf28d841c1b370bc3cea272fc6e9
Adam Holbrook <arholbrook90@gmail.com>
2020-05-07 14:09:24 +0100

    updated about me, though neeeds styling. also somne npm requirements

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -3,18 +3,21 @@ import React from 'react';
 export const About = () => (
   <div>
     <div className="landing-grid" id="about-grid">
-      <div className="blog-header">
-        <div id="header">
-          <div id="logo">About Me</div>
-        </div>
-        <div id="about-me-container">
-          <div id="long-details">
-            <p>I am a hard-working, thoughtful and enthusiastic individual who seeks and enjoys a challenge. I complete all tasks with accuracy, diligence and enjoyment, aiming to produce the best possible results within any given time frame.</p>
-            <p>I have cultured and developed a strong understanding of the testing process. I have had experience working as both a manual and automation engineer, with my daily responsibilities including working with bug databases, test scripts, QA plans and roadmaps, as well as attending meetings with developers and management. I have worked in an agile environment, engaging in scrums and sprint kickoffs.</p>
-            <p>As a manual tester, I have always maintained that communication and detail is key. I have strived in mastering bug databases such as Jira and Targetprocess, entering all details such as bug environments, platforms, steps to reproduce and expected behaviours with diligence. I have also encouraged a Behave Driven Development environment, through which all issues are detailed using Gherkin/Cucumber in order to clearly communicate requirements and concerns at any stage to the whole development team.</p>
-            <p>Whilst learning automative testing, I have become familiar with Selenium Webdriver and Docker, through which I have gained knowledge in application architecture and hierarchies. I have also begun improving my working knowledge of python and javascript through which to build and maintain tests, as well as developing a solid understanding of Git and the command line. I have also become familiar with using AWS in order to create User Pools with Cognito, S3 buckets and handling user permissions, as well as utilising Postman and the command line in order to perform various API testing.</p>
-            <p>Lastly, in my spare time I enjoy keeping fit through running and playing squash, as well as regularly attending the gym. I also have an avid interest in history and computer programming in general. For the latter, I have recently started learning React as to explore different languages, build websites and provide another creative outlet for myself. </p>
-          </div>
+      <div id="header">
+        <div id="logo">About Me</div>
+      </div>
+      <div id="about-me-container">
+        <div id="long-details">
+          <h3>Introduction</h3>
+          <p>Hi there! My names Adam and I'm a QA Automation Engineer living in Brighton, UK.</p>
+          <p>As a graudate from the University of Sussex in History, I have a fine eye for detail and a love of exploration and discovery.</p>
+          <p>I am a hard-working, thoughtful and enthusiastic individual who seeks and enjoys a challenge.</p>
+          <h3>Work</h3>
+          <p>I am a confident and competent QA Engineer, trained in both manual and automation testing practices. I approach problems in a Behave Driven Development learning style, and have become proficient in both python and javascript testing suites, having built both of my own!</p>
+          <p>I have gained skills in API testing, AWS integrations, Selenium Webdriver and Cypress test suites, continuous integration/deployments and containerised environments through Docker. I also have experience as a team lead and mentor to both staff and those in work experience. I have cultivated a love for teaching, and engage with documentation for companies to train with and use as well as developing learning plans for new starters and current staff.</p>
+          <p>I have a ‘can-do’ approach to anything, and love discussing plans and prototypes with brand new technology and software to discover the best way of doing things.</p>
+          <h3>Outside of work</h3>
+          <p>When I'm not working you'll probaly find me being a Dungeon Master, an archer, a model painter, running or cycling over the Sussex Downs, playing games, learning and reading, or chilling with my wife and cat. The cat's name is Tiberius, but we call him Tibbs or Tibblor for short.</p>
         </div>
       </div>
     </div>
